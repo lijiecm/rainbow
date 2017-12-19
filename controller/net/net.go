6e1b445e34669f913370d2bb98f87215ab8468de
lijie <lijie@hys-inc.cn>
2017-12-19 21:33:12 +0800

    完成IP地址分页功能

--- controller/net/net.go
+++ controller/net/net.go
@@ -7,6 +7,41 @@ import (
 	"fmt"
 )
 
+type Page struct {
+    PageNo     int
+    PageSize   int
+    TotalPage  int
+    TotalCount int
+    FirstPage  bool
+    LastPage   bool
+    List       interface{}
+}
+
+func PageUtil(count int, pageNo int, pageSize int, list interface{}) Page {
+	/*
+	pageNo:当前页
+	pageSize:每页的行数
+	count: 总数据行数
+	list: 总数据列表
+	*/
+	/*
+    tp := count / pageSize
+    if count % pageSize > 0 {
+        tp = count / pageSize + 1
+	}
+	star_currentt := (pageNo - 1) * pageSize
+	end_current := pageNo * pageSize
+	lList := list.([]model.Ip)
+	newList := lList[star_currentt:end_current]
+	return Page{TotalPage: tp, TotalCount: count, LastPage: pageNo == tp, List: newList}
+	*/
+	tp := count / pageSize
+    if count % pageSize > 0 {
+        tp = count / pageSize + 1
+    }
+    return Page{PageNo: pageNo, PageSize: pageSize, TotalPage: tp, TotalCount: count, FirstPage: pageNo == 1, LastPage: pageNo == tp, List: list}
+}
+
 type NetController struct {
 	beego.Controller  
 }
@@ -59,10 +94,16 @@ func (p *NetController) Ip(){
 	p.TplName = "net/ip.html"
 
 	errorMsg := "success"
+	pageNo, err := p.GetInt("page")
+	if err != nil {
+		pageNo = 1
+	}
 
-
+	pageSize, err := p.GetInt("page_size")
+	if err != nil {
+		pageSize = 10
+	}
 	
-
 	netWorkModel := model.NewNetWorkModel()
 	ipList, err := netWorkModel.GetAllIp()
 	if err != nil {
@@ -73,12 +114,20 @@ func (p *NetController) Ip(){
 		return
 	}
 
+	count := len(ipList)
+
+	star_currentt := (pageNo - 1) * pageSize
+	end_current := pageNo * pageSize
+	newList := ipList[star_currentt:end_current]
+
+	page := PageUtil(count, pageNo, pageSize, newList) 
 	defer func(){
 		if err != nil {
 			p.Data["error"] = errorMsg
 			p.TplName = "layout/error.html"
 		}
 	}()
-
-	p.Data["ip_list"] = ipList
+	logs.Debug("%v", page)
+	p.Data["Page"] = page
+	return
 	}
--- static/cmdb/net.js
+++ static/cmdb/net.js
@@ -22,23 +22,3 @@ $(function(){
         );
     })
 });
-
-
-
-$('#pageLimit').bootstrapPaginator({
-    currentPage: 1,//当前的请求页面。
-    totalPages: 20,//一共多少页。
-    size:"normal",//应该是页眉的大小。
-    bootstrapMajorVersion: 3,//bootstrap的版本要求。
-    alignment:"right",
-    numberOfPages:5,//一页列出多少数据。
-    itemTexts: function (type, page, current) {//如下的代码是将页眉显示的中文显示我们自定义的中文。
-        switch (type) {
-        case "first": return "首页";
-        case "prev": return "上一页";
-        case "next": return "下一页";
-        case "last": return "末页";
-        case "page": return page;
-        }
-    }
-});
