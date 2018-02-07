0ffb29cc2a8a90530a5e01b317d7b19de284806e
lijie <lijie@hys-inc.cn>
2018-02-07 09:49:46 +0800

    堡垒机添加操作主机日志接口，供堡垒机操作服务器传回输入命令

--- router/api.go
+++ router/api.go
@@ -7,4 +7,5 @@ func init(){
 	beego.Router("/api/getHostInfoBySn", &api.ApiHostController{}, "*:GetHostInfo")
 	beego.Router("/api/updateIpStatus", &api.ApiNetController{}, "*:UpdateIpStatus")
 	beego.Router("/api/getIpAddr", &api.ApiNetController{}, "*:GetIpAddr")
+	beego.Router("/api/addOperaLog", &api.ApiRelayController{}, "*:CreateOperaLog")
 }
